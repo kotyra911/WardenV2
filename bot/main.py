00b26c61c15a6b7c0ced78a4d66faf09bf3c09c2
Климанский Матвей <ak1986tutu@gmail.com>
2025-12-09 12:00:20 +0800

    to sync with 2pc. Finish parse the html

--- bot/main.py
+++ bot/main.py
@@ -1,6 +1,13 @@
 from bot.config import AVITO_URL
 from parser.avito_parser import start_parse
-
 from parser.selenium_driver import driver
+import asyncio
+from aiogram import Bot, Dispatcher
+from aiogram.types import Message
+from aiogram import Router
+
+from config import BOT_TOKEN
+
+
 
 start_parse(driver, AVITO_URL)
--- parser/avito_parser.py
+++ parser/avito_parser.py
@@ -1,3 +1,5 @@
+import time
+
 from selenium.common import NoSuchElementException
 from selenium.webdriver.common.by import By
 from bot.utils.logger import logger
@@ -19,18 +21,30 @@ def start_parse(driver, url:str):
         logger.debug(f"Количество собранных объявлений: {len(items)}")
 
         for item in items:
-            avito_url = item.get_attribute("href")
+            # Скроллинг вниз, для загрузки всех фотографий
+            driver.execute_script("arguments[0].scrollIntoView(true);", item)
+            time.sleep(0.3)
 
-            title = item.find_element(By.CSS_SELECTOR, "[itemprop='name']")
+            # Поиск элемента, который содержит цену
+            price_el = item.find_element(By.CSS_SELECTOR, "span.styles-module-size_l-kPWfk")
+            price = price_el.text
 
+            # Поиск элемента, который содержит ссылку на объявление
+            link_el = item.find_element(By.CSS_SELECTOR, "a")
+            avito_url = link_el.get_attribute("href")
+
+            # Заголовок объявления
+            title = item.find_element(By.CSS_SELECTOR, "[itemprop='name']").text
+
+            # Изъятие фотографии
             block_photo = item.find_element(By.CSS_SELECTOR, ".photo-slider-list-item-r2YDC.photo-slider-dotsCounter-_n_4X")
-
             img = block_photo.find_element(By.TAG_NAME, "img")
+            photo_url = img.get_attribute("src")
 
-            src = img.get_attribute("src")
-
-            print(title.text)
-            print(src)
+            print(avito_url)
+            print(price)
+            print(title)
+            print(photo_url)
 
 
 
